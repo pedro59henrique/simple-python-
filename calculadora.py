5655c5609a7f72766298f170e0edfcee9f4e817e
pedro59henrique <168592485+pedro59henrique@users.noreply.github.com>
2024-05-07 18:07:47 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def sona(a+b):
+  return a+b
+
+if __neme__=="__main__":
+  num1 = float (input("digite o primeiro número:"))
+  num2 = float (input("digite o segundo número:"))
+  resultado = soma (num1,num2)
+  print("resultado:{resultado} f.format (resultado))
